2bc2a9e58bf8071c028c81268259ebbadd254e37
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-05-09 01:24:47 -0300

    agregar funcion

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,11 @@
-var peso = document.getElementById("kilogramos").value;
-var altura = document.getElementById("centimetros").value;
-var resultado;
+function calculadora(){
+	var peso = document.getElementById("kilogramos").value;
+	var altura = document.getElementById("centimetros").value;
+	var resultado;
 
-if(peso!="" && altura!=""){
-	altura = parseInt(altura)/100;
-    resultado = peso/(altura*altura);
-     alert(resultado.toFixed(2));
-};
+	if(peso!="" && altura!=""){
+		altura = parseInt(altura)/100;
+    	resultado = peso/(altura*altura);
+     	alert(resultado.toFixed(2));
+	}
+}
