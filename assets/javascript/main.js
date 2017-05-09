bd03c0bd3718d2705e28b27ded4679c0157c4e05
Diana Villasmil <diacavipi@gmail.com>
2017-05-09 01:27:32 -0300

    refactorizado

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,32 @@
-var peso = document.getElementById("kilogramos").value;
-var altura = document.getElementById("centimetros").value;
-var resultado;
+function calcularIMC(){
+	var peso = leerInput("kilogramos");
+	var altura = leerInput("centimetros");
 
-if(peso!="" && altura!=""){
-	altura = parseInt(altura)/100;
-    resultado = peso/(altura*altura);
-     alert(resultado.toFixed(2));
-};
+	if (validarPesoYAltura(peso, altura)) {
+	    alert(IMC(peso,altura));
+	}
+
+	return false;
+}
+
+function validarBlanco(txt){
+	return txt != '';
+}
+
+function validarPesoYAltura(peso, altura){
+	return validarBlanco(peso) && validarBlanco(altura);
+}
+
+function leerInput(nombre){
+	return document.getElementById(nombre).value;
+}
+
+function EnteroEntre100(valor){
+	return parseInt(valor)/100;
+}
+
+function IMC(peso, altura){
+	return (peso/(Math.pow(EnteroEntre100(altura),2))).toFixed(2);
+}
+
+
