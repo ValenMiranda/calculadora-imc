118937c15ec9300c4d0f97c614cc9b5fa426a063
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-05-09 00:57:09 -0300

    Initial commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+var peso = document.getElementById("kilogramos").value;
+var altura = document.getElementById("centimetros").value;
+var resultado;
+
+if(peso!="" && altura!=""){
+	altura = parseInt(altura)/100;
+    resultado = peso/(altura*altura);
+     alert(resultado.toFixed(2));
+};
